eff399cfc0f9fdcd2eb1f1f51cbcd62243a43b8f
Brian <briandsalemi@gmail.com>
2020-04-06 15:09:17 -0400

    exported robots from robots.js and imported it into index.js. passed id, name, and email from robots as props into Card component. Used those props in Card component to dynamically change the robot information on each Card

--- src/Card.js
+++ src/Card.js
@@ -3,14 +3,14 @@ import React from 'react';
 class Card extends React.Component{
     render(){
         return(
-            <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-                <img alt="robots" src="https://robohash.org/test?200x200"/>
+            <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+                <img alt="robots" src={`https://robohash.org/test${this.props.id}?200x200`}/>
                 <div>
                     <h2>
-                        Jane Doe
+                        {this.props.name}
                     </h2>
                     <p>
-                        jane.doe@gmail.com
+                        {this.props.email}
                     </p>
                 </div>
             </div>
--- src/robots.js
+++ src/robots.js
@@ -61,3 +61,4 @@ export const robots = [
     }
   ];
   
+  export default robots
