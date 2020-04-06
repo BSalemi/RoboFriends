2747c6dfba766552a4b953c58ce8692a0008f441
Brian <briandsalemi@gmail.com>
2020-04-06 12:39:50 -0400

    installed tachyons, deleted App.js file and other unnecessary files. Created Card component and added img, h2, and p tag to it

---
+++ src/Card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Card extends React.Component{
+    render(){
+        return(
+            <div>
+                <img src=""/>
+                <div>
+                    <h2>
+                        Jane Doe
+                    </h2>
+                    <p>
+                        jane.doe@gmail.com
+                    </p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Card
