55b625c4d0b3cea6544eb490b495ce7f617835d2
Brian <briandsalemi@gmail.com>
2020-04-06 14:44:26 -0400

    added a bit more tachyon styling to Card component. created new robots.js file which contains an object of robots that will be used to make Card components dynamic

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+      id: 1,
+      name: 'Lilla Mathias',
+      username: 'Kallistrate',
+      email: 'Genuine@june.biz'
+    },
+    {
+      id: 2,
+      name: 'Jan Victorino',
+      username: 'Hermanika',
+      email: 'sureJan@marsha.tv'
+    },
+    {
+      id: 3,
+      name: 'Melina Wangchuk',
+      username: 'Mokimoki',
+      email: 'rockThe@casbah.net'
+    },
+    {
+      id: 4,
+      name: 'Brick Nataliya',
+      username: 'MateChap',
+      email: 'brickNmortar@tory.org'
+    },
+    {
+      id: 5,
+      name: 'Dharma Elodia',
+      username: 'Tamanna',
+      email: 'dharmaNGreg@fannie.ca'
+    },
+    {
+      id: 6,
+      name: 'Platon Margaux',
+      username: 'Plankton',
+      email: 'chumBucket@bikinibottom.info'
+    },
+    {
+      id: 7,
+      name: 'Mordred Baltazar',
+      username: 'Mormor',
+      email: 'balderdash@gilly.biz'
+    },
+    {
+      id: 8,
+      name: 'Branimira Robert',
+      username: 'Edelgard_12',
+      email: 'blackeagle@garregmach.me'
+    },
+    {
+      id: 9,
+      name: 'Allard Steffen',
+      username: 'Delphine LaLaurie',
+      email: 'kbates@ahs.la'
+    },
+    {
+      id: 10,
+      name: 'Titianus Clay',
+      username: 'AnchoredDown',
+      email: 'clayman@marina.biz'
+    }
+  ];
+  
