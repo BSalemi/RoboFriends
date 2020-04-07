940049377bcfb2f2c9c7693c0d05d6bfa12374f6
Brian <briandsalemi@gmail.com>
2020-04-07 09:04:00 -0400

    created SearchBar component and exported to App. SearchBar has input field for searches to find specific robots

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import CardList from './CardList.js'
+import SearchBar from './SearchBar.js'
 
 const App = () => {
     return(
         <div>
             <h1>RoboFriends</h1>
+            <SearchBar/>
             <CardList/>
         </div>
     )
---
+++ src/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    render(){
+        return(
+            <input type="search" placeholder="Search Robots"/>
+        )
+    }
+}
+
+export default SearchBar
