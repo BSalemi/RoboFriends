878f98bfa45e3379c473df6468e57e1a51925d90
Brian <briandsalemi@gmail.com>
2020-04-07 07:57:04 -0400

    created generateRoboCards function which iterates over robot object to dynamically create RoboCards for each robot

--- src/CardList.js
+++ src/CardList.js
@@ -3,12 +3,18 @@ import Card from './Card.js'
 import { robots } from './robots.js'
 
 class CardList extends React.Component {
+
+    generateRobotCards = () => {
+        const roboCards = robots.map(bot => {
+            return <Card id={bot.id} name={bot.name} email={bot.email}/>
+        })
+        return roboCards
+    }
+
     render(){
         return(
             <div>
-                <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-                <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-                <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+               {this.generateRobotCards()}
             </div>
         )
     }
