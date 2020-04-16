31fc1a78f28639171fc88630312f13f88799c638
Brian <briandsalemi@gmail.com>
2020-04-16 17:22:11 -0400

    created App.css file and imported it in App.js. Added a gradient background to index.css. Added a SEGA.woff file to index.css to change the h1 tag in App. Increased size of header with tachyons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CardList from './CardList.js'
 import SearchBar from './SearchBar.js'
 import { robots } from './robots.js';
+import './App.css';
 
 class App extends React.Component{
 
@@ -24,7 +25,7 @@ class App extends React.Component{
         })
         return(
                 <div className="tc">
-                <h1>RoboFriends</h1>
+                <h1 className="f1">RoboFriends</h1>
                 <SearchBar searchField={this.state.searchField} searchChange={this.onSearchChange}/>
                 <CardList robots={filteredRobots}/>
             </div>
