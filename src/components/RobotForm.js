1642b3965bcb66be3ec4f9f4211c9309389b7026
Brian <briandsalemi@gmail.com>
2020-04-21 09:07:53 -0400

    added state and form input fields and button to RobotForm component

--- src/components/RobotForm.js
+++ src/components/RobotForm.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import robots from '../robots.js'
 
 class RobotForm extends React.Component {
 
+    state = {
+        id: "",
+        name: "",
+        username: "",
+        email: ""
+    }
+
     render(){
         return (
-            <h1>
-                Create a RoboFriend
-            </h1>
+            <>
+                <h1>
+                    Create a RoboFriend
+                </h1>
+                <form>
+                    <input type="text" value={this.state.name}/>
+                    <input type="text" value={this.state.username}/>
+                    <input type="text" value={this.state.email}/>
+                    <button/>
+                </form>
+            </>
         )
     }
 }
